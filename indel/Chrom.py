5840a08346d505b57dc7d4c9308749f7c1f60306
Nicholas Harding <njh@well.ox.ac.uk>
2017-11-07 20:38:01 +0000

    njh hotfix edits

--- indel/Chrom.py
+++ indel/Chrom.py
@@ -5,15 +5,13 @@ import pandas as pd
 
 class Chrom():
     
-    def __init__(self,name,path_to_hdf5):
-        self.name = str(name)
+    def __init__(self, name, path_to_hdf5):
         self.path = path_to_hdf5
-        
-    def read_calls(self):
-        self.callset = h5py.File(self.path,mode='r')
+        self.name = name
+        self.callset = h5py.File(self.path, mode='r')[self.name]
         
     def read_metadata(self,path_to_metadata):
-        self.metadata = pd.read_csv(path_to_metadata, sep='\t')
+        self.metadata = pd.read_table(path_to_metadata)
         
         assert len(self.metadata) == self.callset[self.name]["samples"].shape[0] \
         , "Metadata and callset sample lists are different lengths"
@@ -40,14 +38,17 @@ class Chrom():
         I'll have filtered out some of the positions...)'''
         
         try:
-            self.allPositions = allel.SortedIndex(self.callset[self.name]["variants"]["POS"])
+            self.allPositions = allel.SortedIndex(self.callset["variants"]["POS"])
         except ValueError:
             print("Load one chromosome at a time")
             
+        self.region_feature = feature_table[feature_table.seqid == self.name]
+        assert self.region_feature.shape[0] > 0
+
         self.exonicPositions, self.overlappedFeatures = \
         self.allPositions.locate_intersection_ranges(
-            feature_table[feature_table.seqid == self.name].start,
-            feature_table[feature_table.seqid == self.name].end)
+            self.region_feature.start,
+            self.region_feature.end)
         
     def extract_exonic(self):
         
